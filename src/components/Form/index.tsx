cafc816f5e76ad76491d129b2bcd52e01782cbad
George Kosturov <geosins@yandex.ru>
2022-09-06 23:27:58 +0300

    Add base form with antd

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,26 +1,23 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
+import { Input, Button } from 'antd'
 
 export function Form() {
-    const [data, setData] = useState("")
+  const [tagName, setTagName] = useState("")
+  const [color, setColor] = useState("")
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                padding: 16
-            }}>
-            <h2>
-                Welcome to your{" "}
-                <a href="https://www.plasmo.com" target="_blank">
-                    Plasmo
-                </a>{" "}
-                Extension!
-            </h2>
-            <input onChange={(e) => setData(e.target.value)} value={data} />
-            <a href="https://docs.plasmo.com" target="_blank">
-                View Docs
-            </a>
-        </div>
-    )
+  const changeTagHandler = useCallback((event) => {
+    setTagName(event.target.value)
+  }, [])
+
+  const changeColorHandler = useCallback((event) => {
+    setColor(event.target.value)
+  }, [])
+
+  return (
+    <div>
+      <Input value={tagName} onChange={changeTagHandler} placeholder='Селектор' />
+      <Input value={color} onChange={changeColorHandler} placeholder='Цвет' />
+      <Button>Применить</Button>
+    </div>
+  )
 }

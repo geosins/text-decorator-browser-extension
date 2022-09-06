a6b8d5cddf80b3603df77cd628fb1dc7811f88b4
George Kosturov <geosins@yandex.ru>
2022-09-06 23:36:54 +0300

    Add getCurrentTab util

---
+++ src/utils/getCurrentTab.ts
@@ -0,0 +1,8 @@
+import Tab = chrome.tabs.Tab
+
+export async function getCurrentTab(): Promise<Tab> {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
